a51fdaf5dfb8c6f27b2b1ac0a884973e8dcdf50c
avierkin03 <avierkin03@gmail.com>
2025-07-22 18:59:29 +0300

    added likes and comments

--- tasks/urls.py
+++ tasks/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('<int:pk>/status/', views.TaskChangeStatusView.as_view(), name="task-status"),
     path('<int:pk>/update/', views.TaskUpdateView.as_view(), name="task-update"),
     path('<int:pk>/delete/', views.TaskDeleteView.as_view(), name="task-delete"),
+    path('comment/edit/<int:pk>/', views.CommentUpdateView.as_view(), name='edit_comment'),
+    path('comment/delete/<int:pk>/', views.CommentDeleteView.as_view(), name='delete_comment'),
+    path('comment/like/<int:pk>/', views.CommentLikeToggle.as_view(), name='comment-like-toggle'),
 ]
 
 app_name = "tasks"
